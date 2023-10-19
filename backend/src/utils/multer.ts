ad7f38948348a91764951cc26ff6fd4fd6b7523a
matiash26 <matias.henri26@gmail.com>
2023-10-19 16:49:13 -0300

    fixed: AGORA VAI

--- backend/src/utils/multer.ts
+++ backend/src/utils/multer.ts
@@ -1,11 +1,27 @@
 import * as multer from 'multer';
+import TypeOfMedia from './typeOfMedia';
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'D:/community/www');
-  },
-  filename: function (req, file, cb) {
-    cb(null, '123' + file.originalname);
-  },
-});
-export default storage;
+const multerStorage = function () {
+  let fileName: string;
+  const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, __dirname + '/../public');
+    },
+    filename: function (req, file, cb) {
+      const type = TypeOfMedia(file.originalname);
+      if (type) {
+        const newFileName = `${Date.now() + btoa(file.originalname)}.${type}`;
+        cb(null, newFileName);
+        fileName = newFileName;
+      }
+    },
+  });
+  function getFileName() {
+    return fileName;
+  }
+  function clearFilename() {
+    fileName = null;
+  }
+  return { getFileName, storage, clearFilename };
+};
+export default multerStorage;
