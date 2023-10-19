38a6012da70a3fb1fd83210e8974ca246f9a599f
matiash26 <matias.henri26@gmail.com>
2023-10-19 18:09:20 -0300

    fixed:alinhando o alerta

--- frontend/src/components/Content/AlertScreen.tsx
+++ frontend/src/components/Content/AlertScreen.tsx
@@ -1,6 +1,6 @@
 export default function AlertScreen({ error }: { error: boolean }) {
   return (
-    <h2 style={{ marginTop: '3rem', color: 'white' }}>
+    <h2 style={{ marginTop: '3rem', color: 'white', textAlign:"center" }}>
       {error ? 'Servidor Offline.' : 'Nenhuma postagem no momento.'}
     </h2>
   );
